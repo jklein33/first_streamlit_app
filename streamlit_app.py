be4b13d68b1241c0eb08fb61d6c4ee58b442bed9
jklein33 <110059997+jklein33@users.noreply.github.com>
2022-07-28 13:48:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ fruit_choice = streamlit.text_input("What fruit would you like information about
                                   
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # Normalize
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display data cleanly
